2d0108e571b5235855fd472497d27c88701f62a3
Luke Moscrop <luke.moscrop@bbc.co.uk>
2016-03-02 10:57:28 +0000

    Initial Commit

---
+++ iPlayer Lite/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iPlayer Lite
+//
+//  Created by Luke Moscrop on 02/03/2016.
+//  Copyright © 2016 BBC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iPlayer Lite/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iPlayer Lite
+//
+//  Created by Luke Moscrop on 02/03/2016.
+//  Copyright © 2016 BBC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
